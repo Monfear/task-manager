0fb13ba706808526d6a5e6956f166759a0c36d8a
Patryk Wnukowski <wnuq@ymail.com>
2022-12-16 10:28:03 +0100

    create frontent tasks pages

--- routers/tasksRouter.js
+++ routers/tasksRouter.js
@@ -3,9 +3,12 @@ import { getAllTasks, getSingleTask, updateTask, deleteTask, createTask } from '
 
 const tasksRouter = Router();
 
-tasksRouter.route('/')
+tasksRouter.route('/active')
     .get(getAllTasks)
     .post(createTask);
+
+tasksRouter.route('/completed')
+    .get(getAllTasks);
 
 tasksRouter.route('/task/:id')
     .get(getSingleTask)
